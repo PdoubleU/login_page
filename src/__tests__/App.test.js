789706310ae92376b589b2bdf48975a5510c24e2
PdoubleU <piotrwitasik1988@gmail.com>
2021-08-24 19:07:05 +0200

    [tests]

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 test('renders form', () => {
@@ -26,4 +25,16 @@ test('type nickname in the input', async () => {
   const input = screen.queryByPlaceholderText(/nickname/i);
   fireEvent.change(input, { target: { value: 'JeżJerzy' } });
   expect(input.value).toBe('JeżJerzy');
+});
+
+test('type incorrect nickname', async () => {
+  render(<App />);
+  const input = screen.queryByPlaceholderText(/nickname/i);
+  fireEvent.change(input, { target: { value: '!!' } });
+  fireEvent.click(screen.getByText(/Next/i));
+
+  const result = await screen.findByText(
+    /Nickname/i //need to fix this RegExp, findByText doesnt match multiline string in the doc
+  );
+  expect(result).toBeInTheDocument();
 });
